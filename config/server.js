03afffb8f67c44a9e11ca26bd0a8356fd1933e48
dvdcrc89 <dvdcrc89@gmail.com>
2020-09-07 17:50:33 +0100

    Change server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: "http://localhost:1337",
+  host: env('HOST', process.env.HOST || '0.0.0.0'),
+  port: env.int('PORT', process.env.PORT || 3000),
+  url: "chipsbrighton.herokuapp.com",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
