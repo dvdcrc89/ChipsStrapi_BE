34a8b4113f8a694606f588e7ec73e9b01ce797bf
dvdcrc89 <dvdcrc89@gmail.com>
2020-09-07 17:38:23 +0100

    setup Heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        schema: "public",
-        ssl: { rejectUnauthorized: false }
+  "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "bookshelf",
+      "settings": {
+        "client": "postgres",
+        "host": "${process.env.DATABASE_HOST}",
+        "port": "${process.env.DATABASE_PORT}",
+        "database": "${process.env.DATABASE_NAME}",
+        "username": "${process.env.DATABASE_USERNAME}",
+        "password": "${process.env.DATABASE_PASSWORD}",
+        "ssl": { "rejectUnauthorized": false }
       },
-      options: {}
+      "options": {}
     }
   }
 });
