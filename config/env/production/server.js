5f11c4cea6f5d537ea92c9239602386fafe30c85
dvdcrc89 <dvdcrc89@gmail.com>
2020-09-07 19:18:04 +0100

    heroku build

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  "host": "${process.env.HOST || 0.0.0.0}",
-  "port": "${process.env.PORT || 1337}",
-  "url": "chipsbrighton.herokuapp.com",
-  "admin": {
-    "auth": {
+  host: process.env.HOST,
+  port: process.env.PORT || 1337,
+  url: "chipsbrighton.herokuapp.com",
+  admin: {
+    auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
   },
