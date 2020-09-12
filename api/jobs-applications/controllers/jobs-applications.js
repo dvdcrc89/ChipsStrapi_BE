1ef55ee90ddc2e9878c1f5eec73713cfffe0db62
dvdcrc89 <dvdcrc89@gmail.com>
2020-09-12 14:01:30 +0100

    Toggle Mutation

--- api/jobs-applications/controllers/jobs-applications.js
+++ api/jobs-applications/controllers/jobs-applications.js
@@ -1,4 +1,5 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
@@ -9,17 +10,17 @@ module.exports = {
 
     toggle: async (ctx) =>{
         let entity;
-        const { job_id, user_id } = ctx.request.body;
-        const existingApplication =  await strapi.services['jobs-applications'].find({
-            job: job_id,
-            basic_user:user_id
+        const { basic_user, job } = ctx.request.body;
+        const [existingApplication] =  await strapi.services['jobs-applications'].find({
+            job: job,
+            basic_user:basic_user
         })
         if(existingApplication){
             entity = await strapi.services['jobs-applications'].delete({id: existingApplication.id})
         } else {
             entity = await strapi.services['jobs-applications'].create(ctx.request.body);
         }
-       
+       console.log(entity);
         return sanitizeEntity(entity, { model: strapi.models['jobs-applications'] });
       }
 };
