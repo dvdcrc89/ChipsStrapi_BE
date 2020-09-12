95fdb7f0cdd15c01a5038ba46e5e4ec944c82953
dvdcrc89 <dvdcrc89@gmail.com>
2020-09-12 13:46:03 +0100

    Try to implement Custom Quert

---
+++ api/jobs-applications/config/schema.graphql.js
@@ -0,0 +1,11 @@
+ module.exports = {
+    mutation: `jobsApplicationToggle(input: createJobsApplicationInput): createJobsApplicationPayload`,
+    resolver: {
+      Mutation: {
+        jobsApplicationToggle: {
+          description: 'Create/Delete jobs application',
+          resolver: 'application::jobs-applications.jobs-applications.toggle',
+        },
+      },
+    },
+  };
--- api/jobs-applications/controllers/jobs-applications.js
+++ api/jobs-applications/controllers/jobs-applications.js
@@ -5,4 +5,21 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    toggle: async (ctx) =>{
+        let entity;
+        const { job_id, user_id } = ctx.request.body;
+        const existingApplication =  await strapi.services['jobs-applications'].find({
+            job: job_id,
+            basic_user:user_id
+        })
+        if(existingApplication){
+            entity = await strapi.services['jobs-applications'].delete({id: existingApplication.id})
+        } else {
+            entity = await strapi.services['jobs-applications'].create(ctx.request.body);
+        }
+       
+        return sanitizeEntity(entity, { model: strapi.models['jobs-applications'] });
+      }
+};
